4fdb39103f0c3fff03e6e15563a14b5ebc141962
26903651 <26903651@qq.com>
2016-09-26 09:05:55 +0800

    1

--- src/xlsys/base/XLSYS.java
+++ src/xlsys/base/XLSYS.java
@@ -270,6 +270,10 @@ public interface XLSYS
 	 * 移除关联记录
 	 */
 	public final static String COMMAND_REMOVE_RELATION_RECORD = "_COMMAND_REMOVE_RELATION_RECORD";
+	/**
+	 * 转换视频
+	 */
+	public final static String COMMAND_REMUXING_VIDEO = "_COMMAND_REMUXING_VIDEO";
 	
 	// 以下是Base包中的Session相关常量
 	/**
